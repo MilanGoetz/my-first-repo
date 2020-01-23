fe78fc581e1e2d3ca90f63500520d9b4480c0e14
MilanGoetz <milan.goetz@student.ie.edu>
2020-01-23 15:12:31 +0100

    add file2.py

--- file.py
+++ file.py
@@ -6,5 +6,6 @@ Created on Thu Jan 23 14:08:47 2020
 @author: milangoetz
 """
 
+# this function sums two values
 def func(a, b):
     return a + b
---
+++ file2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 23 15:11:55 2020
+
+@author: milangoetz
+"""
+
